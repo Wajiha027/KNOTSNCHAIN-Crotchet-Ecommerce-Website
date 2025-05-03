f83432184e1e1748495f6be6bfd8d093fb2b4d3d
Anna <annarai1241@gmail.com>
2025-05-04 01:08:15 +0500

    Committ

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
 const {
   authUser,
   registerUser,
   getUserProfile,
   updateUserProfile
 } = require('../controllers/userController');
-
 const { protect } = require('../middleware/auth');
 
+// Correct route definitions
 router.post('/login', authUser);
-router.route('/').post(registerUser);
-router.route('/profile')
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile);
+router.route('/')
+  .post(registerUser);
+  console.log(getUserProfile);  // Add this to check if the function is correctly imported
+
 
 module.exports = router;
